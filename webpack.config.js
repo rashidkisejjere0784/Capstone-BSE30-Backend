76cfa0e57fe1e3c5c662c9411ee44e55f5ccad0f
Kisejjere Rashid <rashidkisejjere0784@gmail.com>
2024-10-15 18:44:06 +0300

    build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './index.js', // Main entry point for your backend code
+  target: 'node', // Target platform is Node.js
+  externals: [nodeExternals()], // Ignore node_modules for backend bundling
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'index.js', // Output bundle file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Transpile JS files with Babel (optional)
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'], // Resolve these extensions
+  },
+  mode: 'production', // Set mode to development
+};

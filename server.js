5e7c2d157028760a3c68798b7d8da69f7e33b1df
Kisejjere Rashid <rashidkisejjere0784@gmail.com>
2024-10-29 22:22:05 +0300

    commit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose
   .catch((error) => console.log('Connection error:', error));
 app.use(
   cors({
-    origin: [process.env.FRONTEND_SERVER, process.env.FRONTEND_SERVER, '*'],
+    origin: '*',
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: [
       'Content-Type',
